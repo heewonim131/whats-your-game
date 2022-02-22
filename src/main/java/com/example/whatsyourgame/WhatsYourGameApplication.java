f63c01597e2d47e0e0deffaa7f1afd0549e6ca62
heewonim131 <heewonim131@gmail.com>
2022-02-22 13:24:41 +0900

    Initial commit

---
+++ src/main/java/com/example/whatsyourgame/WhatsYourGameApplication.java
@@ -0,0 +1,13 @@
+package com.example.whatsyourgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsYourGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatsYourGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/whatsyourgame/WhatsYourGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.whatsyourgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsYourGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
