62beb225cbcbe06d117c9d9f4ef4b8228892f977
heewonim131 <heewonim131@gmail.com>
2022-03-31 18:42:22 +0900

    nginx 설정

--- src/main/java/com/example/whatsyourgame/controller/ProfileController.java
+++ src/main/java/com/example/whatsyourgame/controller/ProfileController.java
@@ -2,13 +2,13 @@ package com.example.whatsyourgame.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 public class ProfileController {
 
